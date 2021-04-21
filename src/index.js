929816c2a382f49c4c3902a38d24a6cf812d5cef
prog.gustavo <prog.gustavosantos@gmail.com>
2021-04-21 18:51:34 -0300

    url

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ var app = require("./app");
 var { nanoid } = require("nanoid");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://gustavo:gustavo@cluster0.qezch.mongodb.net/db_fut?retryWrites=true&w=majority, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://gustavo:gustavo@cluster0.qezch.mongodb.net/db_fut?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //create a server object:
 const port = process.env.PORT || 8080;
